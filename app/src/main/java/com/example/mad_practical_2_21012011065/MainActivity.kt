27fda9012d366a062257de57dfff34c65448cbb3
asus <amipatidar383@gmail.com>
2023-08-03 21:05:03 +0530

    Practical-2

--- app/src/main/java/com/example/mad_practical_2_21012011065/MainActivity.kt
+++ app/src/main/java/com/example/mad_practical_2_21012011065/MainActivity.kt
@@ -1,13 +1,12 @@
 package com.example.mad_practical_2_21012011065
-
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.os.Message
+
 import android.util.Log
 import android.widget.Toast
 import androidx.constraintlayout.widget.ConstraintLayout
 import com.google.android.material.snackbar.Snackbar
-
 class MainActivity : AppCompatActivity() {
     val TAG="MainActivity"
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -15,40 +14,36 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
         showMessage("onCreate method is called.")
     }
-
     override fun onStart() {
         super.onStart()
         showMessage("onStart method is called.")
     }
-
     override fun onResume() {
         super.onResume()
         showMessage("onResume method is called.")
     }
-
     override fun onPause() {
         super.onPause()
         showMessage("onPause method is called.")
     }
-
     override fun onStop() {
         super.onStop()
         showMessage("onStop method is called.")
     }
-
     override fun onDestroy() {
         super.onDestroy()
         showMessage("onDestroy method is called.")
     }
-
     override fun onRestart() {
         super.onRestart()
         showMessage("onRestart method is called.")
     }
     fun showMessage(message: String){
         Log.i(TAG, message)
-        Toast.makeText(this, message, Toast.LENGTH_SHORT).show()
-        val constraint:ConstraintLayout?=findViewById(R.id.mainConstraint)
+        Toast.makeText(this, message,
+            Toast.LENGTH_SHORT).show()
+        val
+                constraint:ConstraintLayout?=findViewById(R.id.mainConstraint)
         if (constraint!=null){
             Snackbar.make(constraint,message,Snackbar.LENGTH_SHORT).show()
         }
